b55539c81810c4e85b224c6612572d582eaf3746
Mikhail Belozerov <belozerov.mg@gmail.com>
2023-12-28 08:43:04 +0700

    modified class Room

--- src/main/java/com/example/hotelbooking/model/Room.java
+++ src/main/java/com/example/hotelbooking/model/Room.java
@@ -1,4 +1,21 @@
 package com.example.hotelbooking.model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private Long id;
+
+    private String roomType;
+
+    private BigDecimal roomPrice;
+
+    private boolean isBooked = false;
+
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
 }
